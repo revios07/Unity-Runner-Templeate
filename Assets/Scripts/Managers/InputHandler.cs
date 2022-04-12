dd711901f1e0ecf3ac51f60b86a36b0da9590353
revios07 <67318384+revios07@users.noreply.github.com>
2022-04-12 21:39:48 +0300

    Update InputHandler.cs

--- Assets/Scripts/Managers/InputHandler.cs
+++ Assets/Scripts/Managers/InputHandler.cs
@@ -22,12 +22,12 @@ public class InputHandler : MonoBehaviour
 
         if (Input.touchCount > 0 && Input.touches[0].phase == TouchPhase.Moved)
         {
-            _xInput += Input.GetTouch(0).deltaPosition.x * Time.deltaTime;
-            _xInput = Mathf.Clamp(_xInput, -_xInputClamp, +_xInputClamp);
+            XInput += Input.GetTouch(0).deltaPosition.x * Time.deltaTime;
+            XInput = Mathf.Clamp(_xInput, -_xInputClamp, +_xInputClamp);
         }
         else
         {
-            _xInput = Mathf.Lerp(_xInput, 0f, 5f * Time.deltaTime);
+            XInput = Mathf.Lerp(_xInput, 0f, 5f * Time.deltaTime);
         }
     }
 }
